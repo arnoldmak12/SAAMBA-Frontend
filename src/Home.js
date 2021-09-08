135f601930131eb57b56c66984f21c017324a924
Sarju Thakkar <thakkar.96@osu.edu>
2021-09-08 07:50:42 -0400

    changed name to saamba

--- src/Home.js
+++ src/Home.js
@@ -9,8 +9,11 @@ function App() {
     <div>
             <div className="d-flex twitter-blue justify-content-center align-items-center text-center content-body">
                 <div>
-                    <h1 className="title-medium mt-5">twitter playlist</h1>
-                    <h2 className="bold">Enter your Twitter Handle</h2>
+                    <h1 className="title-medium mt-5">saamba</h1>
+                    <h2 className="bold">A spotify playlist generator based on your tweets</h2>
+                    <h3 className="px-3">
+                        Enter your twitter handle
+                    </h3>
                     <input className="handle-input"></input>
                     <div className="text-center">
                         <button className="btn-blue" onClick={handleDemo}>
