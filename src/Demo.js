357693a95093c41c8e8a63529e47b6e31dfbae72
ados99 <atharvasdoshi@hotmail.com>
2021-11-16 14:17:52 -0500

    added final touches

--- src/Demo.js
+++ src/Demo.js
@@ -10,60 +10,61 @@ var axios = require('axios').default;
 
 function Playlist(props) {
   let [loading, setLoading] = useState(true);
-  let handle = props.location.state!=null ? props.location.state.handle : "";
+  let handle = props.location.state != null ? props.location.state.handle : "";
   const url = "http://localhost:8080/getPlaylist/";
 
   const storageUris = localStorage.getItem("uris");
   const storageConcepts = localStorage.getItem("concepts");
   const storageTones = localStorage.getItem("tones");
 
-  const [emptyStorage, setEmptyStorage] = useState(storageUris ==null);
+  const [emptyStorage, setEmptyStorage] = useState(storageUris == null);
 
-  const [uris, setUris] = useState(emptyStorage? [] : storageUris.substring(2, storageUris.length-2).split('","'));
-  const [concepts, setConcepts] = useState(emptyStorage? [] : storageConcepts.substring(2, storageConcepts.length-2).split('","'))
-  const [tones, setTones] = useState(emptyStorage? [] : storageTones.substring(2, storageTones.length-2).split('","'))
- 
+  const [uris, setUris] = useState(emptyStorage ? [] : storageUris.substring(2, storageUris.length - 2).split('","'));
+  const [concepts, setConcepts] = useState(emptyStorage ? [] : storageConcepts.substring(2, storageConcepts.length - 2).split('","'))
+  const [tones, setTones] = useState(emptyStorage ? [] : storageTones.substring(2, storageTones.length - 2).split('","'))
+
   const [error, setError] = useState(false);
 
   let params = new URLSearchParams(document.location.search.substring(1));
   let added1 = params.get("added");
-  const [added, setAdded] = useState(added1==="true")
+  const [added, setAdded] = useState(added1 === "true")
   useEffect(() => {
     // var result = "";
-    if(emptyStorage){
-      async function f(){
+    if (emptyStorage) {
+      async function f() {
         await fetch((url + handle), { mode: 'cors' }).then((res) => {
           return fetch(url + handle).then((response) => response.json())
             .then((responseJson) => {
               console.log(res)
               if (res.status == 200) {
-                  setUris(responseJson.trackUris);
-                  setConcepts(responseJson.concepts);
-                  setTones(responseJson.tones);
-                  localStorage.setItem("uris", JSON.stringify(uris));
-                  localStorage.setItem("tones", JSON.stringify(tones)); 
-                  localStorage.setItem("concepts", JSON.stringify(concepts));
-                  setLoading(false);
-            }else {
-              setError(true);
-              setLoading(false);
-            }})
+                setUris(responseJson.trackUris);
+                setConcepts(responseJson.concepts);
+                setTones(responseJson.tones);
+                localStorage.setItem("uris", JSON.stringify(uris));
+                localStorage.setItem("tones", JSON.stringify(tones));
+                localStorage.setItem("concepts", JSON.stringify(concepts));
+                setLoading(false);
+                setEmptyStorage(false);
+              } else {
+                setError(true);
+                setLoading(false);
+              }
+            })
             .catch((error) => {
               console.error(error);
             })
         },
         )
-      
+      }
+      return f();
     }
-    return f();
-    }
-    else{
+    else {
       setLoading(false);
     }
-     
-    }
+
+  }
   );
-  
+
   // error ? 
   return loading ? (
     <div>
@@ -78,76 +79,77 @@ function Playlist(props) {
 
     </div>
   ) :
- 
-    error?
-    <Redirect to={{
-      pathname: '/error'
-    }}> </Redirect>:
-    (
-      <div className="d-flex twitter-blue justify-content-center text-center content-body">
-        <div className="playlist-container">
-          {/* <h1 className = "playlist-ready">Your playlist is ready!</h1> */}
-          <h1 className="title-medium mt-5" style={{ color: "#1ED760" }}>Your playlist is ready!</h1> 
-          {
-            uris.map((uri) => {
-              return (
-                <div className="track-element">
-                  <iframe style={{ display: 'block' }} src={"https://open.spotify.com/embed/track/" + uri.substring(14)} width="100%" height="80" align="right" frameBorder="0"
-                    allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; 
+
+    error ?
+      <Redirect to={{
+        pathname: '/error'
+      }}> </Redirect> :
+      (
+        <div className="d-flex twitter-blue justify-content-center text-center content-body">
+          <div className="playlist-container">
+            {/* <h1 className = "playlist-ready">Your playlist is ready!</h1> */}
+            <h1 className="title-medium mt-5" style={{ color: "#1ED760" }}>Your playlist is ready!</h1>
+            {
+              uris.map((uri) => {
+                return (
+                  <div className="track-element">
+                    <iframe style={{ display: 'block' }} src={"https://open.spotify.com/embed/track/" + uri.substring(14)} width="100%" height="80" align="right" frameBorder="0"
+                      allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; 
         fullscreen; picture-in-picture"></iframe>
-                </div>)
-            }
+                  </div>)
+              }
 
-              // uri =  + uri,
+                // uri =  + uri,
 
-            )}
-          <div class="float-container">
-            <div class="float-child">
-              <button className="btn black" onClick={() => { 
-                window.location = "/"; 
-                localStorage.removeItem("uris");
-                localStorage.removeItem("tones");
-                localStorage.removeItem("concepts");}}>
-                {" "}
-                Back to Home{" "}
-              </button>
+              )}
+            <div class="float-container">
+              <div class="float-child">
+                <button className="btn black" onClick={() => {
+                  window.location = "/";
+                  localStorage.removeItem("uris");
+                  localStorage.removeItem("tones");
+                  localStorage.removeItem("concepts");
+                }}>
+                  {" "}
+                  Back to Home{" "}
+                </button>
+              </div>
+
+              <div class="float-child">
+                <button className="btn green" onClick={() => {
+                  var link = "http://localhost:3005/login?uris=" + uris;
+                  window.location = link
+                  axios.get(link);
+                }}>
+                  {" "}
+                  Add to Spotify{" "}
+                </button>
+              </div>
+              <span class="top3"> Top 3 Concepts: </span>
+              {
+                concepts.map((concept) => {
+                  return <span class="top3content">
+                    {concept + " "}
+                  </span>
+                }
+                  // uri =  + uri,
+
+                )}
+              <div>
+                <span class="top3"> Top 3 Tones: </span>
+                {
+                  tones.map((tone) => {
+                    return <span class="top3content">
+                      {tone + " "}
+                    </span>
+                  }
+                    // uri =  + uri,
+                  )}
+              </div>
             </div>
 
-            <div class="float-child">
-              <button className="btn green" onClick={() => { 
-              var link = "http://localhost:3005/login?uris=" + uris;
-              window.location = link
-              axios.get(link);
-              }}>
-                {" "}
-                Add to Spotify{" "}
-              </button>
-            </div>
-            <span class = "top3"> Top 3 Concepts: </span>
-            {
-              concepts.map((concept) => {               
-                return  <span class = "top3content">
-                  {concept + " "} 
-                </span>
-              }
-              // uri =  + uri,
 
-            )}
-          <div>
-          <span class = "top3"> Top 3 Tones: </span>
-            {
-              tones.map((tone) => {               
-                return  <span class = "top3content">
-                  {tone + " "} 
-                </span>
-              }
-              // uri =  + uri,
-            )}
-          </div>
-          </div>
-          
-            
-{/* {added?
+            {/* {added?
                         <Popup position="right center" modal>
                         {close => 
                           (<div>
@@ -158,11 +160,11 @@ function Playlist(props) {
                           </div>)}
                        </Popup>:""
 } */}
+          </div>
         </div>
-      </div>
 
-      // 
-    )
+        // 
+      )
 }
 
 export default Playlist;
